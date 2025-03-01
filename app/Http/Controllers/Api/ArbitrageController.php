00a1bd1086dae0f88b18c6febaf333c3850c49d9
Akinaru <maxime.utchimata@gmail.com>
2025-03-01 13:22:16 +0100

    Ajout import model account arbitrage

--- app/Http/Controllers/Api/ArbitrageController.php
+++ app/Http/Controllers/Api/ArbitrageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ArbitrageResource;
 use App\Models\Arbitrage;
+use App\Models\Account;
 
 class ArbitrageController extends Controller
 {
