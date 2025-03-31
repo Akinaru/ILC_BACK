e53b1739272715f801b7e84c7adfeb26a9a759f4
Akinaru <maxime.utchimata@gmail.com>
2025-03-31 23:01:44 +0200

    AgreementResource champs null

--- app/Http/Resources/AgreementResource.php
+++ app/Http/Resources/AgreementResource.php
@@ -16,28 +16,57 @@ class AgreementResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'agree_id' => $this->resource->agree_id,
-            'agree_lien' => $this->resource->agree_lien,
-            'agree_nbplace' => $this->resource->agree_nbplace,
-            'agree_typeaccord' => $this->resource->agree_typeaccord,
-            'agree_note' => $this->resource->agree_note,
-            'agree_description' => $this->resource->agree_description,
+            'agree_id' => $this->when($this->resource->agree_id !== null, function () {
+                return $this->resource->agree_id;
+            }, null),
+            
+            'agree_lien' => $this->when($this->resource->agree_lien !== null, function () {
+                return $this->resource->agree_lien;
+            }, null),
+            
+            'agree_nbplace' => $this->when($this->resource->agree_nbplace !== null, function () {
+                return $this->resource->agree_nbplace;
+            }, null),
+            
+            'agree_typeaccord' => $this->when($this->resource->agree_typeaccord !== null, function () {
+                return $this->resource->agree_typeaccord;
+            }, null),
+            
+            'agree_note' => $this->when($this->resource->agree_note !== null, function () {
+                return $this->resource->agree_note;
+            }, null),
+            
+            'agree_description' => $this->when($this->resource->agree_description !== null, function () {
+                return $this->resource->agree_description;
+            }, null),
+            
             'isced' => $this->when($this->resource->isced, function () {
                 return new IscedResource($this->resource->isced);
             }, null),
+            
             'university' => $this->when($this->resource->university, function () {
                 return new UniversityResource($this->resource->university);
             }, null),
+            
             'component' => $this->when($this->resource->component, function () {
                 return new ComponentResource($this->resource->component);
             }, null),
-            'departments' => $this->resource->departments->sortBy('dept_shortname')->values(), // Trie par dept_shortname
-            'partnercountry' => $this->when($this->resource->university && $this->resource->university->partnercountry, function () {
-                return new PartnerCountryResource($this->resource->university->partnercountry);
-            }, null) ?? $this->whenLoaded('partnercountry', function () {
-                return new PartnerCountryResource($this->resource->partnercountry);
-            }),
+            
+            'departments' => $this->when($this->resource->departments !== null && $this->resource->departments->count() > 0, function () {
+                return $this->resource->departments->sortBy('dept_shortname')->values();
+            }, []),
+            
+            'partnercountry' => $this->when(
+                $this->resource->university && $this->resource->university->partnercountry, 
+                function () {
+                    return new PartnerCountryResource($this->resource->university->partnercountry);
+                }, 
+                function () {
+                    return $this->whenLoaded('partnercountry', function () {
+                        return $this->resource->partnercountry ? new PartnerCountryResource($this->resource->partnercountry) : null;
+                    }, null);
+                }
+            ),
         ];
     }
-    
 }
